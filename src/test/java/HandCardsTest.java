4a0a15a67c27185bf240ce1ad4f4a40d03cf802e
T3065 <Cooper.Kai@testritegroup.com>
2023-07-18 18:05:22 +0800

    fix #05 發放手牌，而且手牌必須是13張 -修改13張牌的檢查方法 -增加測試: 有4位玩家產生

--- src/test/java/HandCardsTest.java
+++ src/test/java/HandCardsTest.java
@@ -1,16 +1,21 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class HandCardsTest {
 
 	private static final int THIRTEEN = 13;
-	
+
 	@Test
 	void testGameInitialHandCardsIsThirteen() {
+		// 遊戲開始4人進入
+		BigTwo game = new BigTwo(Arrays.asList(new Player("大壯"), new Player("小帥"), new Player("捲毛"), new Player("小美")));
+
 		// 檢查手牌必須是13張
 		HandCards hand = new HandCards();
-		assertTrue(THIRTEEN == hand.getCardCount());
+		assertEquals(THIRTEEN, hand.getCardCount());
 	}
 
 }
