636aecb60d3fe230c67b29215e37f10454af56ef
cooperkai <eatkaikai@gmail.com>
2023-10-28 17:09:26 +0800

    fix #5 發放手牌，而且手牌必須是13張 -因為測試4人另外寫在 testGameInitialHaveFourPlayer()，所以這個方法只改為測試手牌有沒有13張

--- src/test/java/HandCardsTest.java
+++ src/test/java/HandCardsTest.java
@@ -26,21 +26,17 @@ class HandCardsTest {
 	
 	@Test
 	void testGameInitialHandCardsIsThirteen() {
-		// WHEN 遊戲開始4人進入
-		BigTwo game = new BigTwo(Arrays.asList(new Player("big mussle"), new Player("handsome"), new Player("white"), new Player("beauty")));
-
 		// 牌桌52張牌
 		List<Card> deckCardList = Deck.newDeck();
 
 		// THEN 初始發牌給各花色13張
-		for (Player playerObj : game.getPlayerList()) {
-			HandCards hand = new HandCards();
-			hand.addCards(Deck.deal(deckCardList, HANDCARDS_AMOUNT_THIRTEEN));
-			playerObj.setHandcards(hand);
-			
-			// THEN 檢查手牌必須是13張
-			assertEquals(HANDCARDS_AMOUNT_THIRTEEN, hand.getCardCount());
-		}
+		Player player = new Player("big mussle");
+		HandCards hand = new HandCards();
+		hand.addCards(Deck.deal(deckCardList, HANDCARDS_AMOUNT_THIRTEEN));
+		player.setHandcards(hand);
+
+		// THEN 檢查手牌必須是13張
+		assertEquals(HANDCARDS_AMOUNT_THIRTEEN, hand.getCardCount());
 
 	}
 
